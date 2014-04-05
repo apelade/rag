617b9fe329f8db21487f5b05507fc9dfbf99d82d
Paul McCulloch <hrfeels@gmail.com>
2014-04-05 16:51:03 -0700

    add  config.assets.initialize_on_precompile = false and move new stuff to bottom

--- hw4_grader_stuff/hw4_rottenpotatoes/config/application.rb
+++ hw4_grader_stuff/hw4_rottenpotatoes/config/application.rb
@@ -45,6 +45,12 @@ module Rottenpotatoes
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
@@ -60,11 +66,8 @@ module Rottenpotatoes
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # Fix for Heroku deploy
+    config.assets.initialize_on_precompile = false
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
