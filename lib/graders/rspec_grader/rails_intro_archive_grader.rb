ac9983dbdfc2176309a22478443d56fc8dd24e27
Paul McCulloch <hrfeels@gmail.com>
2014-04-19 14:46:33 -0700

    commented out some items, added comments, before cleanup

--- lib/graders/rspec_grader/rails_intro_archive_grader.rb
+++ lib/graders/rspec_grader/rails_intro_archive_grader.rb
@@ -1,10 +1,13 @@
 require_relative 'heroku_rspec_grader'
+require 'net/http'
+
 
 class RailsIntroArchiveGrader < HerokuRspecGrader
   def initialize(archive, grading_rules)
     super('', grading_rules)
     #TODO make it other than port 3000
-    @heroku_uri = 'http://localhost:3000'
+    #@host_uri = 'http://localhost:3000'
+    @host_uri = 'http://127.0.0.1:3000'
     @archive = archive
   end
 
@@ -21,7 +24,7 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
       puts (cmd +
           @output +
           @errors +
-          @status.to_s) unless @status.success? and @test_errors.empty?
+          @status.to_s) unless @status.success? and @test_errors == ''
 
     # Gets Net:HTTP:Persistent error
     #Open3.popen3(env, cmd) do |stdin, stdout, stderr, wait_thr|
@@ -34,37 +37,16 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
     #end
   end
 
-  def app_loaded?
-    begin
-      require 'net/http'
-      uri = URI.parse("http://127.0.0.1:3000/movies/")
-      response = Net::HTTP.get_response(uri)
-      if response and response.code
-        return true if response.respond_to?(:code) && response.code == '200'
-      end
-    rescue Errno::ECONNREFUSED
-      return false
-    end
-    #return true if `$RAILS_ENV`
-    false
-  end
-
-  def wait_for_app_max(sec, polling=1)
-    to_status = timeout(sec) {
-      sleep(polling) until app_loaded?
-    }
-  end
 
   def grade!
-    ENV['HEROKU_URI'] = @heroku_uri
+    #TODO confirm needed in super
+    ENV['HEROKU_URI'] = @host_uri
 
-    #start_time = Time.now()
     Dir.mktmpdir('rails_intro_archive', '/tmp') do |tmpdir|
 
       @temp = tmpdir
 
-      untar_cmd = "tar -xvf #{@archive} -C /#{@temp}"
-      `#{untar_cmd}`
+      run_process("tar -xvf #{@archive} -C /#{@temp}", '.')
 
       pid = Process.fork do
         run_process('rails s', @temp)
@@ -72,18 +54,42 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
       #Process.detach(pid)
 
       # Gets Net::HTTP::Persistent::Error on local if no timeout, increasing for travis
+      # Max timeout in seconds to wait for rails to respond to http before peppering with tests
       wait_for_app_max(30)
 
       super
 
-      # prev pid no good, get now
-      pid = `pgrep -f "ruby script/rails s"`
+      # prev pid no good?, get now
+      run_process('pgrep -f "ruby script/rails s"', '.')
+      pid = @output
       Process.kill('INT', pid.to_i)
       Process.kill('KILL', pid.to_i)
 
     end
 
+  end
 
+
+  def wait_for_app_max(sec, polling=1)
+    to_status = timeout(sec) {
+      sleep(polling) until app_loaded?
+    }
+  end
+
+  
+  def app_loaded?
+    begin
+      #return true if `pgrep -f "ruby script/rails s"` != ''
+      # Cannot use localhost, use 127.0.0.1
+      uri = URI.parse(@host_uri+'/movies/')
+      response = Net::HTTP.get_response(uri)
+      if response and response.code
+        return true if response.respond_to?(:code) #&& response.code == '200'
+      end
+    rescue Errno::ECONNREFUSED
+      return false
+    end
+    false
   end
 
 
