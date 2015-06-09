a42a9917d595971a39c33d1339f4a157999d6942
Paul McCulloch <hrfeels@gmail.com>
2015-06-09 07:17:08 -0700

    try using travis restart command

--- build_children.sh
+++ build_children.sh
@@ -1,3 +1,5 @@
 # on successful build, build -ci repos which use rag
-BUILD_NUM=$(curl -s 'https://api.travis-ci.com/repos/apelade/rails-intro-ci/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+#BUILD_NUM=$(curl -s 'https://api.travis-ci.com/repos/apelade/rails-intro-ci/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+travis restart -r apelade/rails-intro-ci --pro
+
 
