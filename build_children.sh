f8f801313e02032979ed1b4cad1faafd7fbc59db
Paul McCulloch <hrfeels@gmail.com>
2015-06-09 18:20:57 -0700

    trivial change to trigger build of dependent children

--- build_children.sh
+++ build_children.sh
@@ -2,4 +2,3 @@
 #BUILD_NUM=$(curl -s 'https://api.travis-ci.com/repos/apelade/rails-intro-ci/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 travis restart -r apelade/rails-intro-ci --pro
 
-
